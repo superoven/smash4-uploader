9cbb4c1b0d4b499a68c809b6263df0cc3f2e752c
taylor <taylorritenour@berkeley.edu>
2015-05-07 22:04:38 -0700

    flip characters togglable and tuned correctly - boilerplate refactor

--- client/home.js
+++ client/home.js
@@ -20,28 +20,55 @@ function make_char_url(char_id) {
     return (char_id.length > 0) ? '/img/portraits/' + char_id + '_01.png' : '/img/portraits/omakase_01.png';
 }
 
-function render(player1_img, player1_name, player2_img, player2_name) {
-    //TODO: finish flipping mechanism
+function render(player1_id, player1_name, player2_id, player2_name) {
     images = {
         'bg': '/img/bg.png',
-        'player1': make_char_url(player1_img),
-        'player2': make_char_url(player2_img)
+        'player1': make_char_url(player1_id),
+        'player2': make_char_url(player2_id)
     };
+    function character_render(ctx, character, player_slot_num) {
+        var is_player1 = (player_slot_num == 1);
+        var query = is_player1 ? 'flipLeft' : 'flipRight';
+        var player_id = (is_player1) ? player1_id : player2_id;
+        var portrait_y_axis = 100;
+
+        var should_flip = PortraitOrientations[player_id][query];
+        if (should_flip && !is_player1) {
+            ctx.translate(800, 0);
+            ctx.scale(-1, 1);
+            ctx.drawImage(character, 0, portrait_y_axis);
+            ctx.translate(800, 0);
+            ctx.scale(-1, 1);
+        }
+        else if (should_flip && is_player1) {
+            ctx.scale(-1, 1);
+            ctx.translate(-400, 0);
+            ctx.drawImage(character, 0, portrait_y_axis);
+            ctx.scale(-1, 1);
+            ctx.translate(-400, 0);
+        }
+        else if (!should_flip && is_player1) {
+            ctx.drawImage(character, 0, portrait_y_axis);
+        }
+        else if (!should_flip && !is_player1) {
+            ctx.translate(400, 0);
+            ctx.drawImage(character, 0, portrait_y_axis);
+            ctx.translate(-400, 0);
+        }
+    }
     loadImages(images, function (res) {
         var c = $("#thumbnail")[0];
         var ctx = c.getContext("2d");
         ctx.font = "30px Arial";
         ctx.drawImage(res.bg, 0, 0);
-        ctx.drawImage(res.player1, 0, 30);
+        character_render(ctx, res.player1, 1);
         ctx.fillText(player1_name, 100, 50);
-        ctx.translate(800, 0);
-        ctx.scale(-1, 1);
-        ctx.drawImage(res.player2, 0, 30);
-        ctx.translate(800, 0);
-        ctx.scale(-1, 1);
+        character_render(ctx, res.player2, 2);
         ctx.fillText(player2_name, 550, 50);
     });
 }
+
+
 
 if (Meteor.isClient) {
     Template.Home.helpers({
@@ -77,6 +104,6 @@ Template.Home.events({
     },
     "click .bruh": function (event) {
         var c = $("#thumbnail")[0];
-        Meteor.call("thumbnailUpload", c.toDataURL());
+        Meteor.call("thumbnailUpload", 'E9QGgOqMBGo', c.toDataURL());
     }
 });
--- server/smash-image.js
+++ server/smash-image.js
@@ -1,3 +1,5 @@
+var uuid = Meteor.npmRequire('uuid');
+
 Meteor.methods({
     doYoutube: function () {
         if (! Meteor.userId()) { throw new Meteor.Error("not-authorized"); }
@@ -23,14 +25,10 @@ Meteor.methods({
             snippet: { title: 'New Upload', description: 'Uploaded with ResumableUpload' },
             status: { privacyStatus: 'private' }
         };
-        var uuid = Meteor.npmRequire('uuid');
-        var uploadId = uuid.v1();
 
-        tokens = { access_token: Meteor.user().services.google.accessToken };
-        //var ResumableUpload = Meteor.npmRequire('node-youtube-resumable-upload');
         var ru = new resumableUpload();
-        ru.uploadId = uploadId;
-        ru.tokens = tokens;
+        ru.uploadId = uuid.v1();
+        ru.tokens = { access_token: Meteor.user().services.google.accessToken };
         ru.filepath = '/home/taylor/git/smash4up/deardango.mp4';
         ru.metadata = metadata;
         ru.monitor = true;
@@ -62,21 +60,16 @@ Meteor.methods({
             console.log(err);
         });
     },
-    thumbnailUpload: function (image_data) {
+    thumbnailUpload: function (video_id, image_data) {
         if (! Meteor.userId()) { throw new Meteor.Error("not-authorized"); }
-        var metadata = {};
-        var uuid = Meteor.npmRequire('uuid');
-        var uploadId = uuid.v1();
 
-        tokens = { access_token: Meteor.user().services.google.accessToken };
-        //var ResumableUpload = Meteor.npmRequire('node-youtube-resumable-upload');
         var ru = new resumableUpload();
         ru.is_thumbnail = true;
-        ru.videoId = 'E9QGgOqMBGo';
-        ru.uploadId = uploadId;
-        ru.tokens = tokens;
+        ru.videoId = video_id;
+        ru.uploadId = uuid.v1();
+        ru.tokens = { access_token: Meteor.user().services.google.accessToken };
         ru.image_data = image_data;
-        ru.metadata = metadata;
+        ru.metadata = {};
         ru.monitor = true;
 
         ru.initUpload();
