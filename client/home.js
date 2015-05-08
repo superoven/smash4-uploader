9aad0046040914e9c058959376e99f8011bfc186
taylor <taylorritenour@berkeley.edu>
2015-05-08 01:13:00 -0700

    match type and date added to thumbnail, refactor

--- client/home.js
+++ client/home.js
@@ -20,7 +20,7 @@ function make_char_url(char_id) {
     return (char_id.length > 0) ? '/img/portraits/' + char_id + '_01.png' : '/img/portraits/omakase_01.png';
 }
 
-function render(player1_id, player1_name, player2_id, player2_name) {
+function render(player1_id, player1_name, player2_id, player2_name, match_type, tournament_date) {
     images = {
         'bg': '/img/bg.png',
         'player1': make_char_url(player1_id),
@@ -57,59 +57,73 @@ function render(player1_id, player1_name, player2_id, player2_name) {
         }
     }
     loadImages(images, function (res) {
-        var text_y_axis = 150;
+        var top_text_y_axis = 150;
+        var bottom_text_y_axis = 515;
         var width_of_text_box = 360;
         var c = $("#thumbnail")[0];
         var ctx = c.getContext("2d");
+
+        ctx.drawImage(res.bg, 0, 0);
+
+        character_render(ctx, res.player1, 1);
+        character_render(ctx, res.player2, 2);
+
         ctx.font = "Bolder 74px Impact";
         ctx.fillStyle = "#ffffff";
         ctx.strokeStyle = "#000000";
-        ctx.drawImage(res.bg, 0, 0);
-        character_render(ctx, res.player1, 1);
         ctx.textAlign="start";
-        ctx.fillText(player1_name, 0, text_y_axis, width_of_text_box);
-        ctx.strokeText(player1_name, 0, text_y_axis, width_of_text_box);
-        character_render(ctx, res.player2, 2);
+        ctx.fillText(player1_name, 0, top_text_y_axis, width_of_text_box);
+        ctx.strokeText(player1_name, 0, top_text_y_axis, width_of_text_box);
         ctx.textAlign="end";
-        ctx.fillText(player2_name, 800, text_y_axis, width_of_text_box);
-        ctx.strokeText(player2_name, 800, text_y_axis, width_of_text_box);
+        ctx.fillText(player2_name, 800, top_text_y_axis, width_of_text_box);
+        ctx.strokeText(player2_name, 800, top_text_y_axis, width_of_text_box);
+
+        ctx.font = "Bolder 40px Impact";
+        ctx.textAlign="start";
+        ctx.fillText(match_type, 0, bottom_text_y_axis, width_of_text_box);
+        ctx.strokeText(match_type, 0, bottom_text_y_axis, width_of_text_box);
+        ctx.textAlign="end";
+        ctx.fillText(tournament_date, 800, bottom_text_y_axis, width_of_text_box);
+        ctx.strokeText(tournament_date, 800, bottom_text_y_axis, width_of_text_box);
     });
 }
 
+function doRender() {
+    render($(".player1-character").val(), $(".player1-name").val(), $(".player2-character").val(), $(".player2-name").val(), $(".match-type").val(), $('input.tournament-date').val());
+}
 
+function reRender(event) {
+    doRender();
+    return false;
+}
 
 if (Meteor.isClient) {
     Template.Home.helpers({
         characters: function () {
             return SmashCharacters;
+        },
+        match_types: function () {
+            return MatchTypes;
         }
     });
 
     Template.Home.rendered = function () {
         if (!this._rendered) {
             this._rendered = true;
-            render($(".player1-character").val(), $(".player1-name").val(), $(".player2-character").val(), $(".player2-name").val());
+            this.$('.datetimepicker').datetimepicker({
+                format: 'MM/DD/YYYY'
+            });
+            doRender();
         }
     };
 }
 
 Template.Home.events({
-    "change .player1-character": function (event) {
-        render($(".player1-character").val(), $(".player1-name").val(), $(".player2-character").val(), $(".player2-name").val());
-        return false;
-    },
-    "change .player2-character": function (event) {
-        render($(".player1-character").val(), $(".player1-name").val(), $(".player2-character").val(), $(".player2-name").val());
-        return false;
-    },
-    "change .player1-name": function (event) {
-        render($(".player1-character").val(), $(".player1-name").val(), $(".player2-character").val(), $(".player2-name").val());
-        return false;
-    },
-    "change .player2-name": function (event) {
-        render($(".player1-character").val(), $(".player1-name").val(), $(".player2-character").val(), $(".player2-name").val());
-        return false;
-    },
+    "change .player1-character": reRender,
+    "change .player2-character": reRender,
+    "change .player1-name": reRender,
+    "change .player2-name": reRender,
+    "change .match-type": reRender,
     "click .bruh": function (event) {
         var c = $("#thumbnail")[0];
         Meteor.call("thumbnailUpload", 'E9QGgOqMBGo', c.toDataURL());
